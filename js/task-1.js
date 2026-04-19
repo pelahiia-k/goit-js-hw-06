343cf06b390c0745019bfcb5c1c6aa49422d9c3b
pelahiia <creepygardengnome@gmail.com>
2026-04-19 22:21:26 +0300

    complete 6th hw

--- js/task-1.js
+++ js/task-1.js
@@ -1,9 +1,29 @@
-function makeTransaction(quantity, priceperDroid) {
-  const totalPrice = quantity * priceperDroid;
-  const message = `You ordered ${quantity} droids worth ${totalPrice} credits!`;
-  return message;
-}
+const customer = {
+  username: "Mango",
+  balance: 24000,
+  discount: 0.1,
+  orders: ["Burger", "Pizza", "Salad"],
 
-console.log(makeTransaction(5, 3000)); // "You ordered 5 droids worth 15000 credits!"
-console.log(makeTransaction(3, 1000)); // "You ordered 3 droids worth 3000 credits!"
-console.log(makeTransaction(10, 500)); // "You ordered 10 droids worth 5000 credits!"
+  getBalance() {
+    return this.balance;
+  },
+  getDiscount() {
+    return this.discount;
+  },
+  setDiscount(value) {
+    this.discount = value;
+  },
+  getOrders() {
+    return this.orders;
+  },
+  addOrder(cost, order) {
+    this.balance -= cost - cost * this.discount;
+    this.orders.push(order);
+  },
+};
+
+customer.setDiscount(0.15);
+console.log(customer.getDiscount()); // 0.15
+customer.addOrder(5000, "Steak");
+console.log(customer.getBalance()); // 19750
+console.log(customer.getOrders()); // ["Burger", "Pizza", "Salad", "Steak"]
--- js/task-3.js
+++ js/task-3.js
@@ -1,9 +1,33 @@
-function getElementWidth(content, padding, border) {
-  const boxSizing =
-    parseFloat(content) + parseFloat(padding) * 2 + parseFloat(border) * 2;
-  return boxSizing;
-}
+class StringBuilder {
+  #value;
+  constructor(initialValue) {
+    this.#value = initialValue;
+  }
 
-console.log(getElementWidth("50px", "8px", "4px")); // 74
-console.log(getElementWidth("60px", "12px", "8.5px")); // 101
-console.log(getElementWidth("200px", "0px", "0px")); // 200
+  getValue() {
+    return this.#value;
+  }
+
+  padEnd(str) {
+    this.#value += str;
+    return this;
+  }
+
+  padStart(str) {
+    this.#value = str + this.#value;
+    return this;
+  }
+
+  padBoth(str) {
+    this.#value = str + this.#value + str;
+    return this;
+  }
+}
+const builder = new StringBuilder(".");
+console.log(builder.getValue()); // "."
+builder.padStart("^");
+console.log(builder.getValue()); // "^."
+builder.padEnd("^");
+console.log(builder.getValue()); // "^.^"
+builder.padBoth("=");
+console.log(builder.getValue()); // "=^.^="
